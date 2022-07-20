4cd1fd1f05388cff9a18b1a0b59dc7c62e96d920
Marcin <marcinmosiejko@gmail.com>
2022-07-20 16:38:19 +0200

    Add config file with API url variable

--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,5 @@
+import { API_URL } from './config.js';
+
 export const state = {
   recipe: {},
 };
@@ -7,7 +9,7 @@ export const loadRecipe = async function (id) {
   try {
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb34'
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      `${API_URL}/${id}`
     );
     const data = await res.json();
 
